a1483ab01f37fa6bddf5700b148f0260454fcbc3
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 10:25:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for(var i = 0; i <=25; i++) {
+    if i === 1 {
+      array.push("I am a strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+}
